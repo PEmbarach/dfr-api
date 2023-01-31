9b7eb65098037b046c1a28a528f1da10d21013fb
Pablo Embarach <pabloembarach@gmail.com>
2023-01-31 19:19:57 +0000

    added a post list view, defined a get method to list and create a post

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,10 +6,10 @@ class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(soucer='owner.profile.image.url')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def validate_image(self, value):
-        if value.size > 1024 * 1024 *2:
+        if value.size > 1024 * 1024 * 2:
             raise serializers.ValidationError(
                 'Image size larger than 2MB!'
             )
