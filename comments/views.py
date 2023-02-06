f164571c1187c17e497e73a33b1c78c9f97a4bf1
Pablo Embarach <pabloembarach@gmail.com>
2023-02-06 14:12:27 +0000

    added
    comment filters using DjangoFilterBackend

--- comments/views.py
+++ comments/views.py
@@ -2,12 +2,19 @@ from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSereializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'post'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
