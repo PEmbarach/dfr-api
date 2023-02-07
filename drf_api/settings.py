5a95c5b612a1504b927923f54c2725e3cf8b98d3
Pablo Embarach <pabloembarach@gmail.com>
2023-02-07 11:54:40 +0000

    Update Allowed Hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'True'
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost']
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+]
 
 
 # Application definition
