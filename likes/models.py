33dd69db34e5b28a79b14ab05608db0476d66a33
Pablo Embarach <pabloembarach@gmail.com>
2023-02-18 14:39:33 +0000

    upadate likes app.

--- likes/models.py
+++ likes/models.py
@@ -4,11 +4,14 @@ from posts.models import Post
 
 
 class Like(models.Model):
+    """
+    Like model, related to 'owner' and 'post'.
+    'owner' is a User instance and 'post' is a Post instance.
+    'unique_together' makes sure a user can't like the same post twice.
+    """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(
-        Post,
-        related_name='likes',
-        on_delete=models.CASCADE
+        Post, related_name='likes', on_delete=models.CASCADE
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -17,4 +20,4 @@ class Like(models.Model):
         unique_together = ['owner', 'post']
 
     def __str__(self):
-        return f"{self.owner}{self.post} "
+        return f'{self.owner} {self.post}'
